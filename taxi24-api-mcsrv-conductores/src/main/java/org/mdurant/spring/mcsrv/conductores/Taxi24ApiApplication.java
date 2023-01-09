ad279818ccd13bc63f72f1d156cc56bac9d67f7c
Mauricio Durán <mauriciodurant@gmail.com>
2023-01-08 22:46:05 -0300

    fisrt comment project

---
+++ taxi24-api-mcsrv-conductores/src/main/java/org/mdurant/spring/mcsrv/conductores/Taxi24ApiApplication.java
@@ -0,0 +1,13 @@
+package org.mdurant.spring.mcsrv.conductores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taxi24ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taxi24ApiApplication.class, args);
+	}
+
+}
---
+++ taxi24-api-mcsrv-conductores/src/test/java/org/mdurant/spring/mcsrv/conductores/Taxi24ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mdurant.spring.mcsrv.conductores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taxi24ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
